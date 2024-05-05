a893fda77f7d9ee992edef0cf0aff49747a0d5c1
diwakarKumarSapphire <155961073+diwakarKumarSapphire@users.noreply.github.com>
2024-05-05 17:33:06 +0530

    add animation on card hover

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -14,38 +14,44 @@ export const JobCard = ({ jobDetail }) => {
             <div className="company-details">
                 <div className="company-img" style={{ backgroundImage: `url(${logoUrl})` }}>
                 </div>
-                <div className='role-details'>
-                    <div className='text-md'>{companyName}</div>
-                    <div className='text-md'>{jobRole}</div>
-                    <div className='text-sm'>{location}</div>
+                <div className='role-details primary-text'>
+                    <div className='text-md font-medium'>{companyName}</div>
+                    <div className='text-md font-light'>{jobRole}</div>
+                    <div className='text-sm location-details'>
+                        <span className="material-symbols-outlined text-sm">
+                            location_on
+                        </span>
+                        {location}
+                    </div>
                 </div>
             </div>
-            <div className="estimated-salary text-md">
-                Estimated Salary : {salaryCurrencyCode} {minJdSalary || 0}K - {maxJdSalary}K
+            <div className="estimated-salary text-md font-light">
+                <span className='primary-text'>Estimated Salary : &nbsp;</span>
+                <span className='font-medium secondary-text'>{salaryCurrencyCode} {minJdSalary || 0}K - {maxJdSalary}K</span>
             </div>
             <div className='about-company-container'>
-                <div className='about-company-heading text-l'>About Company:</div>
-                <div className="about-company text-md">
+                <div className='about-company-heading text-l primary-text'>About Company:</div>
+                <div className="about-company text-md font-light secondary-text">
                     {jobDetailsFromCompany}
                 </div>
-                <span className='show-more-btn text-md'>View job</span>
+                <span className='show-more-btn text-md font-regular'>View job</span>
             </div>
             <div className="exp-required text-md">
-                <div>Experience required</div>
-                <div>{minExp || 0} - {maxExp || 0} years</div>
+                <div className='secondary-text font-medium'>Experience required</div>
+                <div className='font-light primary-text'>{minExp || 0} - {maxExp || 0} years</div>
             </div>
             <div className="action-cta-wrapper">
                 <button className="easy-apply-btn action-btn">
-                    <span className="material-symbols-outlined easy-apply-icon">
+                    <span className="material-symbols-outlined easy-apply-icon text-md">
                         electric_bolt
                     </span>
-                    <span className='text-md'>Easy Apply</span>
+                    <span className='text-l font-medium primary-text'>Easy Apply</span>
                 </button>
                 <button className="unlock-referral-btn action-btn">
                     <span className="material-symbols-outlined">
                         account_circle
                     </span>
-                    <span className='text-md'>Unlock Referral Asks</span>
+                    <span className='text-l font-medium'>Unlock Referral Asks</span>
                 </button>
             </div>
         </div>
